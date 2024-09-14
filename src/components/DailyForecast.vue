02c201769b4fee23814b94bf19b9e66dcab54f5e
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-09-14 15:27:50 +0200

    add descriptions to forecast

--- src/components/DailyForecast.vue
+++ src/components/DailyForecast.vue
@@ -1,23 +1,37 @@
 <template>
   <div>
     <h1>Weather Data</h1>
-    <pre>{{ daily }}</pre>
+    <pre>{{ combinedWeatherDataWithDescriptions }}</pre>
   </div>
-  <div v-for="(data, code) in descriptionsData" :key="code">
+
+  <!-- <div v-for="(data, code) in descriptionsData" :key="code">
     {{ code }}
     <img :src="data.day.image" />
     <img :src="data.night.image" />
-  </div>
+  </div> -->
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import { useStore } from 'vuex';
 import descriptions from '../assets/descriptions.json';
 
 const store = useStore();
 
-const descriptionsData = ref(descriptions);
-
 const daily = computed(() => store.getters['weather/daily']);
+
+const combinedWeatherData = computed(() =>
+  daily.value.time.map((date, index) => ({
+    date: date,
+    weatherCode: daily.value.weather_code[index],
+  }))
+);
+
+const combinedWeatherDataWithDescriptions = computed(() =>
+  combinedWeatherData.value.map((day) => ({
+    ...day,
+    day: descriptions[day.weatherCode].day,
+    night: descriptions[day.weatherCode].night,
+  }))
+);
 </script>
