e9389a043d14eb0a3d7b64847b337fa64b29460a
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-09-14 17:21:34 +0200

    add styling

--- src/components/DailyForecast.vue
+++ src/components/DailyForecast.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    <h1>Weather Data</h1>
-    <pre>{{ combinedWeatherDataWithDescriptions }}</pre>
-  </div>
+  <ul>
+    <li v-for="weather in dailyWeatherData" :key="weather.date">
+      <img :src="weather.day.image" :alt="weather.day.description" />
+      <h3>{{ getWeekday(weather.date) }}</h3>
+      <p>
+        <i class="fa-solid fa-caret-up"></i>
+        <span>{{ weather.maxTemp }}&deg;C</span>
+      </p>
+      <p>
+        <i class="fa-solid fa-caret-down"></i>
+        <span>{{ weather.minTemp }}&deg;C</span>
+      </p>
+    </li>
+  </ul>
 </template>
 
 <script setup>
@@ -14,14 +24,53 @@ const store = useStore();
 
 const daily = computed(() => store.getters['weather/daily']);
 
-const combinedWeatherDataWithDescriptions = computed(() =>
+const dailyWeatherData = computed(() =>
   daily.value.time.map((date, index) => {
     const weatherCode = daily.value.weather_code[index];
     return {
       date: date,
       day: descriptions[weatherCode].day,
       night: descriptions[weatherCode].night,
+      maxTemp: daily.value.temperature_2m_max[index],
+      minTemp: daily.value.temperature_2m_min[index],
     };
   })
 );
+
+const getWeekday = (dateString) => {
+  const weekdays = ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za'];
+  const day = new Date(dateString);
+  return weekdays[day.getDay()];
+};
 </script>
+
+<style scoped>
+ul {
+  display: flex;
+  list-style: none;
+  gap: 1rem;
+}
+
+li {
+  text-align: center;
+  border: 1px lightgray solid;
+  padding: 1rem;
+  border-radius: 5px;
+}
+
+img {
+  max-width: 100px;
+  max-height: 100px;
+}
+
+h3 {
+  margin-bottom: 1rem;
+}
+
+p {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 1rem;
+}
+</style>
--- src/store/modules/weather.js
+++ src/store/modules/weather.js
@@ -30,7 +30,7 @@ const actions = {
   },
 
   async fetchWeather({ commit }, { latitude, longitude }) {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,is_day,weather_code&hourly=temperature_2m,weather_code&daily=weather_code&timezone=auto`;
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,is_day,weather_code&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`;
     try {
       const response = await fetch(url);
       if (!response.ok) {
