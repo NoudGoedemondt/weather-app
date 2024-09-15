180b970e71d8b8a62e90cc07a1d428e09e27d37c
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-09-15 11:46:21 +0200

    add base html

--- src/components/CurrentForecast.vue
+++ src/components/CurrentForecast.vue
@@ -1,21 +1,25 @@
 <template>
   <div v-if="weatherData">
     <h1>{{ city }}</h1>
-    <pre>
-      {{ currentWeather }}
-    </pre>
+    <img :src="iconUrl" alt="current weather icon" />
+    <h2>{{ currentWeather.temperature_2m }}&deg;C</h2>
   </div>
 </template>
 
 <script setup>
 import { computed } from 'vue';
 import { useStore } from 'vuex';
+import descriptions from '../assets/descriptions.json';
 
 const store = useStore();
 
 const weatherData = computed(() => store.state.weather.weatherData);
 
 const city = computed(() => store.getters['weather/city']);
-
 const currentWeather = computed(() => store.getters['weather/current']);
+
+const iconUrl = computed(() => {
+  const index = currentWeather.value.weather_code;
+  return descriptions[index].day.image;
+});
 </script>
